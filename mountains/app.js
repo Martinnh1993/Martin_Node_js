c468bf4008d4ff89a0894e9385a187667ac915bb
Martinnh1993 <90694207+Martinnh1993@users.noreply.github.com>
2023-09-10 14:38:53 +0200

    rest of CRUD made

--- mountains/app.js
+++ mountains/app.js
@@ -9,21 +9,66 @@ const app = express();
         { id: 5, name: 'Makalu', height: 8485 },
     ];
    
-    app.get('/mountains', (req, res) => {
-        res.send(mountains);
-    });
+    // GET all mountains
+app.get('/mountains', (req, res) => {
+  res.send(mountains);
+});
 
-    app.get('/mountains/:id', (req, res) => {
-        const mountainId = parseInt(req.params.id);
-        const mountain = mountains.find((m) => m.id === mountainId);
-      
-        if (!mountain) {
-          res.status(404).send({ error: 'Mountain not found' });
-        } else {
-          res.send(mountain);
-        }
-    });
+// GET a specific mountain by ID
+app.get('/mountains/:id', (req, res) => {
+  const mountainId = parseInt(req.params.id);
+  const mountain = mountains.find((m) => m.id === mountainId);
 
+  if (!mountain) {
+    res.status(404).send({ error: 'Mountain not found' });
+  } else {
+    res.send(mountain);
+  }
+});
 
+// POST a new mountain
+app.post('/mountains', (req, res) => {
+  const newMountain = req.body;
+  if (!newMountain.name || !newMountain.height) {
+    res.status(400).send({ error: 'Name and height are required' });
+  } else {
+    const nextId = mountains.length + 1;
+    newMountain.id = nextId;
+    mountains.push(newMountain);
+    res.status(201).send(newMountain);
+  }
+});
+
+// PUT (update) a mountain by ID
+app.put('/mountains/:id', (req, res) => {
+  const mountainId = parseInt(req.params.id);
+  const updatedMountain = req.body;
+
+  const existingMountain = mountains.find((m) => m.id === mountainId);
+  if (!existingMountain) {
+    res.status(404).send({ error: 'Mountain not found' });
+  } else {
+    if (updatedMountain.name) {
+      existingMountain.name = updatedMountain.name;
+    }
+    if (updatedMountain.height) {
+      existingMountain.height = updatedMountain.height;
+    }
+    res.send(existingMountain);
+  }
+});
+
+// DELETE a mountain by ID
+app.delete('/mountains/:id', (req, res) => {
+  const mountainId = parseInt(req.params.id);
+  const index = mountains.findIndex((m) => m.id === mountainId);
+
+  if (index === -1) {
+    res.status(404).send({ error: 'Mountain not found' });
+  } else {
+    const deletedMountain = mountains.splice(index, 1);
+    res.send(deletedMountain[0]);
+  }
+});
 
 app.listen(8080);
