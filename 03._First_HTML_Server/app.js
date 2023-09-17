595bf1d39c50d383392ed9200680f30941d3983e
Martinnh1993 <90694207+Martinnh1993@users.noreply.github.com>
2023-09-17 18:40:23 +0200

    html

---
+++ 03._First_HTML_Server/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/home.html");
+});
+
+app.get("/secondPage", (req, res) => {
+    res.sendFile(__dirname + "/public/secondPage.html");
+});
+
+app.get("/welcomeMessage", (req, res) => {
+    const clientName = req.query.user;
+    if (!clientName) {
+        res.send({data: "Hello stranger"})
+    } else {
+        res.send({data: `Welcome to my facy website, ${clientName}` })
+    }
+})
+
+
+
+const PORT = 8080;
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log("Server failed to start", error);
+        return;
+    }
+    console.log("Server is running on port", PORT);
+});
