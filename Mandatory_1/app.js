ad76db65c509142de4868f714fee40ae34b86ae3
Martinnh1993 <90694207+Martinnh1993@users.noreply.github.com>
2023-10-05 12:50:00 +0200

    intial commit for mandatory 1

---
+++ Mandatory_1/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/login.html");
+});
+
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + "/public/home.html");
+});
+
+var users = [
+    {
+        username: 'admin',
+        password: 'password'
+    },
+    {
+        username: 'user1',
+        password: '1234'
+    }
+];
+
+function check(req, res) {
+    const { userid, pswrd } = req.query; // Get username and password from the query string
+    const user = users.find(user => user.username === userid && user.password === pswrd);
+
+    if (user) {
+        res.redirect("/home.html"); // Redirect to the second page on successful login
+    } else {
+        res.send("Error: Incorrect Password or Username"); // Send an error message
+    }
+}
+
+app.get("/login", check);
+
+const PORT = 8080;
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log("Server failed to start", error);
+        return;
+    }
+    console.log("Server is running on port", PORT);
+});
